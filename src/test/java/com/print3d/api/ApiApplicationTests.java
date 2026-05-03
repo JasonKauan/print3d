ba3ee07a815a0df3b3ed21a37175747bf4710ef8
JasonKauan <jasonkauandiass@gmail.com>
2026-05-03 07:21:39 -0300

    first commit

---
+++ src/test/java/com/print3d/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.print3d.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
